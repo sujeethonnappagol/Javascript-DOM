4a53bb6238335993a824921a7d92bb6120022b7a
sujeet <sujeethonnappagol3@gmail.com>
2025-09-18 00:56:45 +0530

    Done with DOM

---
+++ app.js
@@ -0,0 +1,21 @@
+// function locked(){
+//     let heading = document.getElementById('heading')
+//     heading.innerText = "congratulations! you have successfully completed all the previous lectures of javascript, Sujeet"
+//     let btn = document.getElementById('btn')
+//     btn.style.display = 'none'
+// }
+// locked()
+
+// function welcome(){
+//     let StudentName = document.getElementById('name')
+//     let namespan = document.getElementById('namespan')
+//     namespan.innerText = StudentName.value
+//     StudentName.value = ""
+// }
+// welcome()
+
+function split(){
+    let amount = document.getElementById('amount');
+    let person = document.getElementById('person');
+    let resultdiv = amount.value / person.value;
+}
